fcfa24b25bd002551d90ffbf4d44e8df6aa177ef
yusuf-inci <yusufinci@protonmail.com>
2023-04-07 00:19:12 +0300

    added userdata

---
+++ userdata/jenkins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install fontconfig openjdk-11-jre -y
+sudo apt-get install jenkins -y
+sudo apt install maven -y
